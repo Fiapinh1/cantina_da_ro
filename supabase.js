18e16c8c64ea101cfc1920c14ae489c26efb36e8
Fiapinh1 <david.ccalmeida@gmail.com>
2026-05-13 15:22:19 -0300

    Atualizar credenciais do Supabase

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://hxqcejkoqmivlqffjqzq.supabase.co';
+const SUPABASE_URL = 'sb_publishable_ivzyV2UdvN7hpV6KFOX5_g_Islb5hEU';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4cWNlamtvcW1pdmxxZmZqcXpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzkwNDksImV4cCI6MjA5NDI1NTA0OX0.oHXgXmjJuM8SqH2nwIOzsJH5HEQnM9ApbRom2SqTwLA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
